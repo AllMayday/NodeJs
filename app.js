c9dd43a09cd88238b8ace266449ff4d74b3a7def
Caio Almeida <caiofacchidev@gmail.com>
2024-11-25 06:54:20 -0300

    Beggining to work with Express.Js Replaced app.js with index.js to comply with Express.Js standards

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(requisicao,resposta){
-  resposta.end("Welcome!");
-}).listen(8181);
-
-console.log("Server up");
---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express"); 
+const app = express();              
+
+// Apenas importar não é suficiente. É necessário iniciar o express 
+// em uma variável para que ele possa ser utilizado no projeto
