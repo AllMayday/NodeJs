4ed68e1ae9078ed024403f286b773d24fa8d0534
Caio Almeida <caiofacchidev@gmail.com>
2024-11-25 07:13:36 -0300

    Changed writing to english for better consistency Learning routes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require("express"); 
 const app = express();              
 
-// Apenas importar não é suficiente. É necessário iniciar o express 
-// em uma variável para que ele possa ser utilizado no projeto
+// It isn't enough toi just import Express. It is necessary to initialize it 
+// in a variable so it may be used, as you'll see above
+
+app.get("/",function(req,res){
+    res.send("Response caught");
+    // Response is always necessary in a route, else the function
+    // keeps reloading itself, never returning anything to user
+    // .send ENDS THE CONNECTION
+    // You cannot send two responses
+});
+
+
+
+
+
+
+
+app.listen(4000, function(erro){
+  if(erro){
+    console.log("Something's wrong");
+  }
+  else{
+    console.log("Server up")
+  }
+})
