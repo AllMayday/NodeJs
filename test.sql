a2b90a3767e7a3c7b4947d5143d87aca17da241d
Caio Almeida <caiofacchidev@gmail.com>
2024-11-30 12:49:41 -0300

    This is just a SQL file to write SQL commands beforehand. MySQL is being used, thus, all queries are being put in cmd direcly. Saving this draft so I remember to use it

---
+++ test.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+    user_name VARCHAR(50),
+    user_login VARCHAR(50),
+    user_email VARCHAR(100),
+    user_age int
+);
+
+INSERT INTO users
+values('Mary', 'Mariiiiiie', 'Mary@gmail.com', 29)
+
+SELECT * FROM users
+
+UPDATE users
+SET User_age = 35
+where user_name = 'Mary'
